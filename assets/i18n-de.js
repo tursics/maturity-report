ae426423ea671e136b23e16dabaf415ca08a8240
Thomas Tursics <thomas@tursics.de>
2024-02-02 22:04:08 +0100

    first step to translate to German

--- assets/i18n-de.js
+++ assets/i18n-de.js
@@ -1,5 +1,8 @@
 var i18n = i18n || [];
 i18n['de'] = {
+    'debug': 'Debug',
+    'EU': 'EU',
+    'Info': 'Info',
     'loadFile': 'Lade Datei',
     'odm_report': 'Bericht zum Reifegrad offener Daten',
 };
--- assets/i18n-en.js
+++ assets/i18n-en.js
@@ -1,5 +1,8 @@
 var i18n = i18n || [];
 i18n['en'] = {
+    'debug': 'Debug',
+    'EU': 'EU',
+    'Info': 'Info',
     'loadFile': 'Load file',
     'odm_report': 'Open Data Maturity Report',
 };

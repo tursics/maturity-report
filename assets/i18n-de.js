63aff59f73001370868c85fb10f47c8101a1c76b
Thomas Tursics <thomas@tursics.de>
2024-02-18 23:54:52 +0100

    translate sidebar text

--- assets/i18n-de.js
+++ assets/i18n-de.js
@@ -1,9 +1,14 @@
 var i18n = i18n || [];
 i18n['de'] = {
     'countries': 'Staaten:',
+    'D0': 'Dimension 0: Informationen',
     'debug': 'Debug',
     'EU': 'EU',
+    'guide': 'Anleitung zum antworten',
+    'headline': 'Überschrift',
     'Info': 'Info',
     'loadFile': 'Lade Datei',
+    'notes': 'Anmerkungen',
     'odm_report': 'Bericht zum Reifegrad offener Daten',
+    'question': 'Frage',
 };
--- assets/i18n-en.js
+++ assets/i18n-en.js
@@ -1,9 +1,14 @@
 var i18n = i18n || [];
 i18n['en'] = {
     'countries': 'Countries:',
+    'D0': 'Dimension 0: Information',
     'debug': 'Debug',
     'EU': 'EU',
+    'guide': 'Guide to answering',
+    'headline': 'Headline',
     'Info': 'Info',
     'loadFile': 'Load file',
+    'notes': 'Notes',
     'odm_report': 'Open Data Maturity Report',
+    'question': 'Question',
 };
